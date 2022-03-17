c4a6344a4879e9ccfc3342f49f7e4512ddc6b299
Daniel Liu <66093915+danlliu@users.noreply.github.com>
2022-03-17 14:03:28 -0400

    Added final animations and education section.

--- js/index.js
+++ js/index.js
@@ -1,29 +1,37 @@
 
-SECTIONS = [$('#technical-experience'), $('#education'), $('#awards'), $('#connect'), $('#more-about-me')]
+const SECTIONS = [$('#technical-experience'), $('#education'), $('#awards'), $('#connect'), $('#more-about-me')]
+let DO_ANIMATION = true;
 
 function animateBlinker() {
-    setInterval(() => {
+    if (!DO_ANIMATION) { return; }
+    let intervalID = setInterval(() => {
+        if (!DO_ANIMATION) {clearInterval(intervalID);return;}
         $('h1.terminal-prompt').toggleClass('prompt-shown');
     }, 750);
 }
 
 function animateSections() {
+    if (!DO_ANIMATION) { return; }
     let i = 0;
     let intervalID = setInterval(() => {
+        if (!DO_ANIMATION) {clearInterval(intervalID);return;}
         SECTIONS[i++].addClass('shown');
         if (i === SECTIONS.length) {
             clearInterval(intervalID);
+            $('#skip-animation-button').addClass('hidden');
         }
     }, 400);
 }
 
 function animateTerminal() {
-    const input = $('h1.terminal')
+    if (!DO_ANIMATION) { return; }
+    const input = $('h2.terminal')
     const output = $('p.terminal')
 
     // typing 1: "hello world"
     let contents = '$ ';
     let intervalID = setInterval(() => {
+        if (!DO_ANIMATION) { clearInterval(intervalID); return; }
         if (contents === '$ hello world') {
             input.removeClass('terminal-prompt');
             setTimeout(() => {
@@ -37,6 +45,15 @@ function animateTerminal() {
         console.log(contents);
         input.html(contents);
     }, 250);
+}
+
+function skipAnimation() {
+    DO_ANIMATION = false;
+    $('h2.terminal').html('$ hello world')
+    $('p.terminal').html('Welcome to my website!<br>Process finished with exit code HELLO');
+    for (let section of SECTIONS) {
+        section.addClass('shown');
+    }
 }
 
 animateBlinker();
